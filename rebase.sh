ca68b9b683778e6c666af65496635665195c1bb4
Александр Морозов <=>
2023-01-25 16:02:13 -0500

    change of main

--- rebase.sh
+++ rebase.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
 # display command line options
 
+
+
 count=1
-for param in "$*"; do
-	echo "\$* Parameter #$count = $param"
-	count=$(( $count + 1 ))
-done
+
+for param in "$@"; do
+
+	    echo "\$@ Parameter #$count = $param"
+
+	        count=$(( $count + 1 ))
+
+	done
+
+
+
+	echo "====="
